ba30ea616e7bee7181692e942b91df1c241135bc
Seu Nome <seuemail@email.com>
2025-12-17 20:48:09 -0300

    Fix build scripts for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repo = "my-blog";
-
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-
-  // GitHub Pages publica em /<repo>
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-
   images: {
-    unoptimized: true,          // GitHub Pages não suporta otimização do next/image
-    qualities: [75, 100],       // remove teu warning do quality={100}
+    qualities: [75, 100],
   },
 };
 
